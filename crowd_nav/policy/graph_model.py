8c48e5ebda41b4e118f644eadeb306636dbd4f6e
zhouzhiqian <zhiqian.zhou17@gmail.com>
2021-01-11 22:05:16 +0800

    [wip] try our attention transformer

--- crowd_nav/policy/graph_model.py
+++ crowd_nav/policy/graph_model.py
@@ -47,6 +47,8 @@ class RGL(nn.Module):
         elif self.similarity_function == 'concatenation':
             self.w_a = mlp(2 * X_dim, [2 * X_dim, 1], last_relu=True)
 
+        self.w_v = mlp(X_dim, [X_dim], last_relu=True)
+
         # TODO: try other dim size
         embedding_dim = self.X_dim
         self.Ws = torch.nn.ParameterList()
@@ -114,9 +116,10 @@ class RGL(nn.Module):
         # compute matrix A
         if not self.layerwise_graph:
             normalized_A = self.compute_similarity_matrix(X)
+            value_X = self.w_v(X)
             self.A = normalized_A[0, :, :].data.cpu().numpy()
 
-        next_H = H = X
+        next_H = H = value_X
         for i in range(self.num_layer):
             if self.layerwise_graph:
                 A = self.compute_similarity_matrix(H)
@@ -131,6 +134,145 @@ class RGL(nn.Module):
         return next_H
 
 class GAT_RL(nn.Module):
+    def __init__(self, config, robot_state_dim, human_state_dim):
+        """ The current code might not be compatible with models trained with previous version
+        """
+        super().__init__()
+        self.multiagent_training = config.gcn.multiagent_training
+        num_layer = config.gcn.num_layer
+        X_dim = config.gcn.X_dim
+        wr_dims = config.gcn.wr_dims
+        wh_dims = config.gcn.wh_dims
+        final_state_dim = config.gcn.final_state_dim
+        similarity_function = config.gcn.similarity_function
+        layerwise_graph = config.gcn.layerwise_graph
+        skip_connection = config.gcn.skip_connection
+        self.leakyrelu = nn.LeakyReLU(negative_slope=-0.2)
+
+        # design choice
+
+        # 'gaussian', 'embedded_gaussian', 'cosine', 'cosine_softmax', 'concatenation'
+        self.similarity_function = similarity_function
+        self.robot_state_dim = robot_state_dim
+        self.human_state_dim = human_state_dim
+        self.num_layer = num_layer
+        self.X_dim = X_dim
+        self.layerwise_graph = layerwise_graph
+        self.skip_connection = skip_connection
+
+        logging.info('Similarity_func: {}'.format(self.similarity_function))
+        logging.info('Layerwise_graph: {}'.format(self.layerwise_graph))
+        logging.info('Skip_connection: {}'.format(self.skip_connection))
+        logging.info('Number of layers: {}'.format(self.num_layer))
+
+        self.w_r = mlp(robot_state_dim, wr_dims, last_relu=True)
+        self.w_h = mlp(human_state_dim, wh_dims, last_relu=True)
+
+        if self.similarity_function == 'embedded_gaussian':
+            self.w_a = Parameter(torch.randn(self.X_dim, self.X_dim))
+            nn.init.orthogonal_(self.w_a.data)
+        elif self.similarity_function == 'concatenation':
+            self.w_a = mlp(2 * X_dim, [2 * X_dim, 1], last_relu=True)
+
+        self.w_v = mlp(X_dim, [X_dim], last_relu=True)
+
+        # TODO: try other dim size
+        embedding_dim = self.X_dim
+        self.Ws = torch.nn.ParameterList()
+        for i in range(self.num_layer):
+            if i == 0:
+                self.Ws.append(Parameter(torch.randn(self.X_dim, embedding_dim)))
+            elif i == self.num_layer - 1:
+                self.Ws.append(Parameter(torch.randn(embedding_dim, final_state_dim)))
+            else:
+                self.Ws.append(Parameter(torch.randn(embedding_dim, embedding_dim)))
+
+        # for visualization
+        self.A = None
+
+    def compute_adjectory_matrix(self, state):
+        robot_state = state[0]
+        human_state = state[1]
+        robot_num = robot_state.size()[1]
+        human_num = human_state.size()[1]
+        Num = robot_num + human_num
+        adj = torch.ones((Num, Num))
+        for i in range(robot_num, robot_num+human_num):
+            adj[i][0] = 0
+        adj = adj.repeat(robot_state.size()[0], 1, 1)
+        return adj
+
+    def compute_similarity_matrix(self, X):
+        if self.similarity_function == 'embedded_gaussian':
+            A = torch.matmul(torch.matmul(X, self.w_a), X.permute(0, 2, 1))
+            # normalized_A = softmax(A, dim=2)
+        elif self.similarity_function == 'gaussian':
+            A = torch.matmul(X, X.permute(0, 2, 1))
+            # normalized_A = softmax(A, dim=2)
+        elif self.similarity_function == 'cosine':
+            A = torch.matmul(X, X.permute(0, 2, 1))
+            magnitudes = torch.norm(A, dim=2, keepdim=True)
+            norm_matrix = torch.matmul(magnitudes, magnitudes.permute(0, 2, 1))
+            normalized_A = torch.div(A, norm_matrix)
+        elif self.similarity_function == 'cosine_softmax':
+            A = torch.matmul(X, X.permute(0, 2, 1))
+            magnitudes = torch.norm(A, dim=2, keepdim=True)
+            norm_matrix = torch.matmul(magnitudes, magnitudes.permute(0, 2, 1))
+            normalized_A = softmax(torch.div(A, norm_matrix), dim=2)
+        elif self.similarity_function == 'concatenation':
+            indices = [pair for pair in itertools.product(list(range(X.size(1))), repeat=2)]
+            selected_features = torch.index_select(X, dim=1, index=torch.LongTensor(indices).reshape(-1))
+            pairwise_features = selected_features.reshape((-1, X.size(1) * X.size(1), X.size(2) * 2))
+            A = self.w_a(pairwise_features).reshape(-1, X.size(1), X.size(1))
+            # normalized_A = softmax(A, dim=2)
+            normalized_A = A
+        elif self.similarity_function == 'squared':
+            A = torch.matmul(X, X.permute(0, 2, 1))
+            squared_A = A * A
+            normalized_A = squared_A / torch.sum(squared_A, dim=2, keepdim=True)
+        elif self.similarity_function == 'equal_attention':
+            normalized_A = (torch.ones(X.size(1), X.size(1)) / X.size(1)).expand(X.size(0), X.size(1), X.size(1))
+        elif self.similarity_function == 'diagonal':
+            normalized_A = (torch.eye(X.size(1), X.size(1))).expand(X.size(0), X.size(1), X.size(1))
+        else:
+            raise NotImplementedError
+
+        return normalized_A
+
+    def forward(self, state):
+        """
+        Embed current state tensor pair (robot_state, human_states) into a latent space
+        Each tensor is of shape (batch_size, # of agent, features)
+        :param state:
+        :return:
+        """
+        robot_state, human_states = state
+        adj = self.compute_adjectory_matrix(state)
+        # compute feature matrix X
+        robot_state_embedings = self.w_r(robot_state)
+        human_state_embedings = self.w_h(human_states)
+        X = torch.cat([robot_state_embedings, human_state_embedings], dim=1)
+        # value_X = self.w_v(X)
+        # compute matrix A
+        if not self.layerwise_graph:
+            normalized_A = self.compute_similarity_matrix(X)
+
+            self.A = normalized_A[0, :, :].data.cpu().numpy()
+
+        next_H = H = X
+        A = self.compute_similarity_matrix(H)
+        e = self.leakyrelu(A)
+        zero_vec = -9e15 * torch.ones_like(e)
+        attention = torch.where(adj > 0, e, zero_vec)
+        attention = nn.functional.softmax(attention, dim=2)
+        next_H = relu(torch.matmul(torch.matmul(attention, self.w_v(X)), self.Ws[0]))
+
+        if self.skip_connection:
+            next_H += H
+
+        return next_H
+
+class GAT_RL2(nn.Module):
     def __init__(self, config, robot_state_dim, human_state_dim, device):
         """ The current code might not be compatible with models trained with previous version
         """
