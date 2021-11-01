9a25c121c9b3b27ce608877f9c08c07148407db7
zhouzhiqian <540894485@qq.com>
2021-11-01 09:07:58 +0800

    [wip] replace leakyrelu with relu

--- crowd_nav/policy/helpers.py
+++ crowd_nav/policy/helpers.py
@@ -13,8 +13,8 @@ def mlp(input_dim, mlp_dims, last_relu=False):
         layers.append(nn.Linear(mlp_dims[i], mlp_dims[i + 1]))
         # nn.init.orthogonal(layers[-1].weight)
         if i != len(mlp_dims) - 2 or last_relu:
-            # layers.append(nn.ReLU())
-            layers.append(nn.LeakyReLU(negative_slope=0.1))
+            layers.append(nn.ReLU())
+            # layers.append(nn.LeakyReLU(negative_slope=0.1))
     net = nn.Sequential(*layers)
     return net
 
