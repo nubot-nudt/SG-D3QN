[wip] fix an error in rotation process

crowd_nav/policy/value_estimator.py
@@ -168,15 +168,19 @@ class DQNNetwork(nn.Module):
             dy = robot_state[:, :, 6] - robot_state[:, :, 1]
             dx = dx.unsqueeze(1)
             dy = dy.unsqueeze(1)
-            radius_r = robot_state[:, :, 4].unsqueeze(1)
+
             dg = torch.norm(torch.cat([dx, dy], dim=2), 2, dim=2, keepdim=True)
             rot = torch.atan2(dy, dx)
             cos_rot = torch.cos(rot)
             sin_rot = torch.sin(rot)
             transform_matrix = torch.cat((cos_rot, -sin_rot, sin_rot, cos_rot), dim=1).reshape(batch, 2, 2)
             robot_velocities = torch.bmm(robot_state[:, :, 2:4], transform_matrix)
+            radius_r = robot_state[:, :, 4].unsqueeze(1)
+            v_pref = robot_state[:, :, 7].unsqueeze(1)
+            target_heading = torch.zeros_like(radius_r)
             pos_r = torch.zeros_like(robot_velocities)
-            new_robot_state = torch.cat((pos_r, robot_velocities, radius_r, dg, rot, robot_state[:, :, 7:9]), dim=2)
+            cur_heading = robot_state[:, :, 8].unsqueeze(1) - rot
+            new_robot_state = torch.cat((pos_r, robot_velocities, radius_r, dg, target_heading, v_pref, cur_heading), dim=2)
             human_positions = human_state[:, :, 0:2] - robot_state[:, :, 0:2]
             human_positions = torch.bmm(human_positions, transform_matrix)
             human_velocities = human_state[:, :, 2:4]