f04c39676fdd34f29a1b39d84e8157c96fc45c24
zhiqianzhou <540894485@qq.com>
2021-11-04 15:09:40 +0800

    [wip] train on ali cloud

--- crowd_nav/train_on_server.py
+++ crowd_nav/train_on_server.py
@@ -11,10 +11,11 @@ import git
 import re
 from tensorboardX import SummaryWriter
 from crowd_sim.envs.utils.robot import Robot
-from crowd_nav.utils.trainer import VNRLTrainer, MPRLTrainer
+from crowd_nav.utils.trainer import VNRLTrainer, MPRLTrainer, TSRLTrainer, TD3RLTrainer
 from crowd_nav.utils.memory import ReplayMemory
 from crowd_nav.utils.explorer import Explorer
 from crowd_nav.policy.policy_factory import policy_factory
+from crowd_nav.policy.reward_estimate import Reward_Estimator
 
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import plot, savefig
@@ -34,7 +35,10 @@ def main(args):
     # configure paths
     make_new_dir = True
     if os.path.exists(args.output_dir):
-        shutil.rmtree(args.output_dir)
+        if args.overwrite:
+            shutil.rmtree(args.output_dir)
+        else:
+            shutil.rmtree(args.output_dir)
     if make_new_dir:
         os.makedirs(args.output_dir)
         shutil.copy(args.config, os.path.join(args.output_dir, 'config.py'))
@@ -75,6 +79,11 @@ def main(args):
                         format='%(asctime)s, %(levelname)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
     repo = git.Repo(search_parent_directories=True)
     logging.info('Current git head hash code: {}'.format(repo.head.object.hexsha))
+    logging.info('Current random seed: {}'.format(sys_args.randomseed))
+    logging.info('Current safe_weight: {}'.format(sys_args.safe_weight))
+    logging.info('Current goal_weight: {}'.format(sys_args.goal_weight))
+    logging.info('Current re_collision: {}'.format(sys_args.re_collision))
+    logging.info('Current re_arrival: {}'.format(sys_args.re_arrival))
     logging.info('Current config content is :{}'.format(config))
     device = torch.device("cuda:0" if torch.cuda.is_available() and args.gpu else "cpu")
     logging.info('Using device: %s', device)
@@ -90,11 +99,25 @@ def main(args):
 
     # configure environment
     env_config = config.EnvConfig(args.debug)
+    env_config.reward.collision_penalty = args.re_collision
+    env_config.reward.success_reward = args.re_arrival
+    env_config.reward.goal_factor = args.goal_weight
+    env_config.reward.discomfort_penalty_factor = args.safe_weight
+    env_config.sim.human_num = args.human_num
+
     env = gym.make('CrowdSim-v0')
     env.configure(env_config)
     robot = Robot(env_config, 'robot')
     robot.time_step = env.time_step
     env.set_robot(robot)
+
+    reward_estimator = Reward_Estimator()
+    reward_estimator.configure(env_config)
+    policy.reward_estimator = reward_estimator
+    # for continous action
+    action_dim = env.action_space.shape[0]
+    max_action = env.action_space.high
+    min_action = env.action_space.low
 
     # read training parameters
     train_config = config.TrainConfig(args.debug)
@@ -115,15 +138,30 @@ def main(args):
     model = policy.get_model()
     batch_size = train_config.trainer.batch_size
     optimizer = train_config.trainer.optimizer
+    print(policy_config.name)
     if policy_config.name == 'model_predictive_rl':
         trainer = MPRLTrainer(model, policy.state_predictor, memory, device, policy, writer, batch_size, optimizer, env.human_num,
                               reduce_sp_update_frequency=train_config.train.reduce_sp_update_frequency,
                               freeze_state_predictor=train_config.train.freeze_state_predictor,
                               detach_state_predictor=train_config.train.detach_state_predictor,
                               share_graph_model=policy_config.model_predictive_rl.share_graph_model)
+    elif policy_config.name == 'tree_search_rl':
+        trainer = TSRLTrainer(model, policy.state_predictor, memory, device, policy, writer, batch_size, optimizer, env.human_num,
+                              reduce_sp_update_frequency=train_config.train.reduce_sp_update_frequency,
+                              freeze_state_predictor=train_config.train.freeze_state_predictor,
+                              detach_state_predictor=train_config.train.detach_state_predictor,
+                              share_graph_model=policy_config.model_predictive_rl.share_graph_model)
+
     elif policy_config.name == 'gat_predictive_rl':
         trainer = MPRLTrainer(model, policy.state_predictor, memory, device, policy, writer, batch_size, optimizer, env.human_num,
                               reduce_sp_update_frequency=train_config.train.reduce_sp_update_frequency,
+                              freeze_state_predictor=train_config.train.freeze_state_predictor,
+                              detach_state_predictor=train_config.train.detach_state_predictor,
+                              share_graph_model=policy_config.model_predictive_rl.share_graph_model)
+    elif policy_config.name == 'td3_rl':
+        policy.set_action(action_dim, max_action, min_action)
+        trainer = TD3RLTrainer(policy.actor, policy.critic, policy.state_predictor, memory, device, policy, writer,
+                              batch_size, optimizer, env.human_num, reduce_sp_update_frequency=train_config.train.reduce_sp_update_frequency,
                               freeze_state_predictor=train_config.train.freeze_state_predictor,
                               detach_state_predictor=train_config.train.detach_state_predictor,
                               share_graph_model=policy_config.model_predictive_rl.share_graph_model)
@@ -132,37 +170,37 @@ def main(args):
     explorer = Explorer(env, robot, device, writer, memory, policy.gamma, target_policy=policy)
     policy.save_model(in_weight_file)
     # imitation learning
-    if args.resume:
-        if not os.path.exists(rl_weight_file):
-            logging.error('RL weights does not exist')
-        policy.load_state_dict(torch.load(rl_weight_file))
-        model = policy.get_model()
-        rl_weight_file = os.path.join(args.output_dir, 'resumed_rl_model.pth')
-        logging.info('Load reinforcement learning trained weights. Resume training')
-    elif os.path.exists(il_weight_file):
-        policy.load_state_dict(torch.load(rl_weight_file))
-        model = policy.get_model()
-        logging.info('Load imitation learning trained weights.')
-    else:
-        il_episodes = train_config.imitation_learning.il_episodes
-        il_policy = train_config.imitation_learning.il_policy
-        il_epochs = train_config.imitation_learning.il_epochs
-        il_learning_rate = train_config.imitation_learning.il_learning_rate
-        trainer.set_learning_rate(il_learning_rate)
-        if robot.visible:
-            safety_space = 0
-        else:
-            safety_space = train_config.imitation_learning.safety_space
-        il_policy = policy_factory[il_policy]()
-        il_policy.set_common_parameters(policy_config)
-        il_policy.multiagent_training = policy.multiagent_training
-        il_policy.safety_space = safety_space
-        robot.set_policy(il_policy)
-        explorer.run_k_episodes(il_episodes, 'train', update_memory=True, imitation_learning=True)
-        trainer.optimize_epoch(il_epochs)
-        policy.save_model(il_weight_file)
-        logging.info('Finish imitation learning. Weights saved.')
-        logging.info('Experience set size: %d/%d', len(memory), memory.capacity)
+    # if args.resume:
+    #     if not os.path.exists(rl_weight_file):
+    #         logging.error('RL weights does not exist')
+    #     policy.load_state_dict(torch.load(rl_weight_file))
+    #     model = policy.get_model()
+    #     rl_weight_file = os.path.join(args.output_dir, 'resumed_rl_model.pth')
+    #     logging.info('Load reinforcement learning trained weights. Resume training')
+    # elif os.path.exists(il_weight_file):
+    #     policy.load_state_dict(torch.load(rl_weight_file))
+    #     model = policy.get_model()
+    #     logging.info('Load imitation learning trained weights.')
+    # else:
+    #     il_episodes = train_config.imitation_learning.il_episodes
+    #     il_policy = train_config.imitation_learning.il_policy
+    #     il_epochs = train_config.imitation_learning.il_epochs
+    #     il_learning_rate = train_config.imitation_learning.il_learning_rate
+    #     trainer.set_learning_rate(il_learning_rate)
+    #     if robot.visible:
+    #         safety_space = 0
+    #     else:
+    #         safety_space = train_config.imitation_learning.safety_space
+    #     il_policy = policy_factory[il_policy]()
+    #     il_policy.set_common_parameters(policy_config)
+    #     il_policy.multiagent_training = policy.multiagent_training
+    #     il_policy.safety_space = safety_space
+    #     robot.set_policy(il_policy)
+    #     explorer.run_k_episodes(il_episodes, 'train', update_memory=True, imitation_learning=True)
+    #     trainer.optimize_epoch(il_epochs)
+    #     policy.save_model(il_weight_file)
+    #     logging.info('Finish imitation learning. Weights saved.')
+    #     logging.info('Experience set size: %d/%d', len(memory), memory.capacity)
 
 
     trainer.update_target_model(model)
@@ -193,8 +231,18 @@ def main(args):
 
     episode = 0
     reward_rec = []
+    return_rec = []
+    discom_tim_rec = []
+    nav_time_rec = []
+    total_time_rec = []
     reward_in_last_interval = 0
+    return_in_last_interval = 0
+    nav_time__in_last_interval = 0
+    discom_time_in_last_interval = 0
+    total_time_in_last_interval = 0
     eps_count = 0
+    # fw = open(sys_args.output_dir + '/data.txt', 'w')
+    # print("%f %f %f %f %f" % (0,0,0,0,0), file=fw)
     while episode < train_episodes:
         if args.resume:
             epsilon = epsilon_end
@@ -206,17 +254,32 @@ def main(args):
         robot.policy.set_epsilon(epsilon)
 
         # sample k episodes into memory and optimize over the generated memory
-        _, _, _, sum_reward, _ = explorer.run_k_episodes(sample_episodes, 'train', update_memory=True, episode=episode)
+        _, _, nav_time, sum_reward, ave_return, discom_time, total_time = \
+            explorer.run_k_episodes(sample_episodes, 'train', update_memory=True, episode=episode)
         eps_count = eps_count + 1
         reward_in_last_interval = reward_in_last_interval + sum_reward
+        return_in_last_interval = return_in_last_interval + ave_return
+        nav_time__in_last_interval = nav_time__in_last_interval + nav_time
+        discom_time_in_last_interval = discom_time_in_last_interval + discom_time
+        total_time_in_last_interval = total_time_in_last_interval + total_time
         interval = 100
         if eps_count % interval == 0:
-            reward_rec.append(reward_in_last_interval/100)
-
-            logging.info('Train in episode %d reward in last 100 episodes %f', eps_count, reward_rec[-1])
-            reward_in_last_interval = 0.0
-            min_reward = int((np.min(reward_rec) // 10)*10)
-            max_reward = int((np.max(reward_rec) // 10 + 1)*10)
+            reward_rec.append(reward_in_last_interval/100.0)
+            return_rec.append(return_in_last_interval/100.0)
+            discom_tim_rec.append(discom_time_in_last_interval/100.0)
+            nav_time_rec.append(nav_time__in_last_interval/100.0)
+            total_time_rec.append(total_time_in_last_interval/100.0)
+            logging.info('Train in episode %d reward in last 100 episodes %f %f %f %f %f', eps_count, reward_rec[-1],
+                         return_rec[-1], discom_tim_rec[-1], nav_time_rec[-1], total_time_rec[-1])
+            # print("%f %f %f %f %f" % (reward_rec[-1], return_rec[-1], discom_tim_rec[-1], nav_time_rec[-1],
+            #                          total_time_rec[-1]), file=fw)
+            reward_in_last_interval = 0
+            return_in_last_interval = 0
+            nav_time__in_last_interval = 0
+            discom_time_in_last_interval = 0
+            total_time_in_last_interval = 0
+            min_reward = (np.min(reward_rec) // 5.0 ) * 5.0
+            max_reward = (np.max(reward_rec) // 5.0 + 1) * 5.0
             pos = np.array(range(1, len(reward_rec)+1)) * interval
             plt.plot(pos, reward_rec, color='r', marker='.', linestyle='dashed')
             plt.axis([0, eps_count, min_reward, max_reward])
@@ -230,7 +293,7 @@ def main(args):
             trainer.update_target_model(model)
         # evaluate the model
         if episode % evaluation_interval == 0:
-            _, _, _, reward, _ = explorer.run_k_episodes(env.case_size['val'], 'val', episode=episode)
+            _, _, _, reward, _, _, _ = explorer.run_k_episodes(env.case_size['val'], 'val', episode=episode)
             explorer.log('val', episode // evaluation_interval)
 
             if episode % checkpoint_interval == 0 and reward > best_val_reward:
@@ -247,6 +310,7 @@ def main(args):
             policy.save_model(save_every_checkpoint_rl_weight_file)
 
     # # test with the best val model
+    # fw.close()
     if best_val_model is not None:
         policy.load_state_dict(best_val_model)
         torch.save(best_val_model, os.path.join(args.output_dir, 'best_val.pth'))
@@ -256,8 +320,8 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Parse configuration file')
-    parser.add_argument('--policy', type=str, default='model_predictive_rl')
-    parser.add_argument('--config', type=str, default='configs/icra_benchmark/mp_separate.py')
+    parser.add_argument('--policy', type=str, default='tree_search_rl')
+    parser.add_argument('--config', type=str, default='configs/icra_benchmark/ts_separate.py')
     parser.add_argument('--output_dir', type=str, default='data/output1')
     parser.add_argument('--overwrite', default=False, action='store_true')
     parser.add_argument('--weights', type=str)
@@ -265,7 +329,13 @@ if __name__ == '__main__':
     parser.add_argument('--gpu', default=False, action='store_true')
     parser.add_argument('--debug', default=False, action='store_true')
     parser.add_argument('--test_after_every_eval', default=False, action='store_true')
-    parser.add_argument('--randomseed', type=int, default=17)
+    parser.add_argument('--randomseed', type=int, default=7)
+    parser.add_argument('--human_num', type=int, default=5)
+    parser.add_argument('--safe_weight', type=float, default=1.0)
+    parser.add_argument('--goal_weight', type=float, default=0.2)
+    parser.add_argument('--re_collision', type=float, default=-0.5)
+    parser.add_argument('--re_arrival', type=float, default=1.0)
+
 
     # arguments for GCN
     # parser.add_argument('--X_dim', type=int, default=32)
